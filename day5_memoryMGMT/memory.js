22bfbc7dc8a7abded978da5145269c1381eb6106
Anmol Srivastav <anmolsri520@gmail.com>
2025-12-12 01:23:42 +0530

    error

--- day5_memoryMGMT/memory.js
+++ day5_memoryMGMT/memory.js
@@ -1,6 +1,9 @@
-/*const v8 = require('node:v8');
+const v8 = require('node:v8');
 const { heap_size_limit } = v8.getHeapStatistics();
 const heapSizeInGB = heap_size_limit / (1024 * 1024 * 1024);
-console.log(`${heapSizeInGB} GB`);*/
+console.log(`${heapSizeInGB} GB`);
 
 console.log(process.memoryUsage());
+
+//At its core, V8 divides memory into several parts,
+//  with two primary areas being the heap and the stack.
